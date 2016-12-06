d1dd2a234cf6a2b3848f4bb5ae43d46837db44f7
Selma Dorrestein <selmadorrestein@gmail.com>
2016-12-06 19:13:22 +0100

    admins can now change their email, their password and can add another admin

--- routes/adm-addadmin.js
+++ routes/adm-addadmin.js
@@ -61,5 +61,76 @@ router.post('/adm-addadmin', function (req, res) {
 	}
 })
 
+//change e-mail address
+router.post('/adm-changeemail', (req, res) => {
+	//In case frontend validation breaks, backend validation to make sure form is not empty
+	if (req.body.newemail !== 0) {
+		//Check to find out if the new email adress already exists in the database, assigned to a different user
+		db.Admin.findOne({
+			where: {
+				email: req.body.newemail.toLowerCase()
+			}
+		}).then( (admin) => {
+			//If it already exists, it can not be changed
+			if(admin) {
+				res.redirect('/adm-addadmin?message=' + encodeURIComponent('Sorry, an other admin already uses this e-mail address.'))
+				return
+				//Otherwise: change it!
+			} else {
+				db.Admin.findOne({
+					where: {
+						email: req.session.user.email
+					}
+				}).then( (admin) => {
+					admin.updateAttributes({
+						email: req.body.newemail.toLowerCase(),
+					})
+					req.session.user = admin
+					res.redirect('/adm-addadmin?message=' + encodeURIComponent('Your email has been changed.'))
+					return
+				})
+			}
+		})
+	}
+})
+
+//Update password
+router.post('/adm-changepassword', (req, res) => {
+	//In case front end validation breaks, make sure new password is at least 8 characters
+	if(req.body.newpassword.length <= 7) {
+		res.redirect('adm-addadmin?message=' + encodeURIComponent("Your password should be at least 8 characters long. Please try again."));
+		return;
+		//Then check whether user didn't make a typo in choosing a new password
+	} else if(req.body.newpassword !== req.body.newpassword2) {
+		res.redirect('adm-addadmin?message=' + encodeURIComponent("Your passwords did not match. Please try again."));
+		return;
+	} else {
+		db.Admin.findOne({
+			where: {
+				id: req.session.user.id
+			}
+			//Since changing a password is a heavy change, first the old password is required. The old password needs to match the one in the database.
+		}).then( (admin) => {
+			bcrypt.compare(req.body.oldpassword, admin.password, (err, result) => {
+				if(result) {
+					//If the user succesfully passed all this validation, a new hashed password will be made.
+					bcrypt.hash(req.body.newpassword, null, null, function(err, hash) {
+						if (err) throw (err)
+							admin.updateAttributes({
+								password: hash,
+							})
+					})
+					req.session.user	= admin
+					res.redirect('/adm-addadmin?message=' + encodeURIComponent('Your password has been changed.'))
+					return
+				} else {
+					res.redirect('/adm-addadmin?message=' + encodeURIComponent('Your old password is incorrect. Please try again.'))
+					return
+				}
+			})
+		})
+	}
+})
+
 //// Export
 module.exports = router
